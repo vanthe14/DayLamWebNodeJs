b1d883c9f508221569b47f56b4f60c113184077a
vanthe14@gmail.com <levanthe14020506>
2017-04-24 15:02:11 +0700

    upcode level 2

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
+var famousPage={
+  image:['img/gallery/slider-img-1.jpg',
+         'img/gallery/slider-img-2.jpg',
+         'img/gallery/slider-img-3.jpg',
+         'img/gallery/slider-img-4.jpg'
+       ],
+  title: 'Welcome to NodeJS.',
+  contentLead:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pretium vulputate magna sit amet blandit.',
+  content:'Cras rutrum, massa non blandit convallis, est lacus gravida enim, eu fermentum ligula orci et tortor. In sit amet nisl ac leo pulvinar molestie. Morbi blandit ultricies ultrices.',
+  refReadmore:'/'
+};
 
+var allPage=[{
+  data_id: 'id-1',
+  hrefImageFull:'gallery-img-1-full.jpg',
+  hrefPage:'#',
+  hrefImageCol:'gallery-img-1-4col.jpg'
+},
+{
+  data_id: 'id-2',
+  hrefImageFull:'gallery-img-1-full.jpg',
+  hrefPage:'#',
+  hrefImageCol:'gallery-img-1-4col.jpg'
+},
+{
+  data_id: 'id-3',
+  hrefImageFull:'gallery-img-1-full.jpg',
+  hrefPage:'#',
+  hrefImageCol:'gallery-img-1-4col.jpg'
+},
+{
+  data_id: 'id-4',
+  hrefImageFull:'gallery-img-1-full.jpg',
+  hrefPage:'#',
+  hrefImageCol:'gallery-img-1-4col.jpg'
+},
+{
+  data_id: 'id-5',
+  hrefImageFull:'gallery-img-1-full.jpg',
+  hrefPage:'#',
+  hrefImageCol:'gallery-img-1-4col.jpg'
+}
+];
 router.get('/', function(req, res) {
-    res.render("pages/home");
+    res.render("pages/home",{idMenu:1,famousPage:famousPage,allPage:allPage});
 });
 
 router.post('/', function(req, res) {
--- routes/loadPage.js
+++ routes/loadPage.js
@@ -1,6 +1,6 @@
 var pageId="page-contact"
 
 module.exports.loadPage = function(req, res, next) {
-    console.log(req.params.id);
-    res.render("pages/"+pageId);
+  //  console.log(req.params.id);
+    res.render("pages/"+pageId,{idMenu:7});
 }
